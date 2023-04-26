a748d4e1e6c1b0066ee56f161a449c9b6275846f
Chris Otto <chris.otto6@gmail.com>
2023-04-26 13:57:23 -0500

    Updating

---
+++ pages/api/og.js
@@ -0,0 +1,24 @@
+import { ImageResponse } from '@vercel/og'
+
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function () {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        Hello, World!
+      </div>
+    )
+  )
+}
